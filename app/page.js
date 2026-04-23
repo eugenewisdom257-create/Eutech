01083dd2f2d65b1a1f175522b4eef3af120ddcd5
eugenewisdom257-create <eugenewisdom257@gmail.com>
2026-04-23 13:39:23 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>EuTech School System</h1>
+      <p>The operating system for modern schools.</p>
+
+      <button style={{ padding: 10, marginTop: 20 }}>
+        Request Demo
+      </button>
+    </div>
+  );
+}
