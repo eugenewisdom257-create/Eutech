97c26c49f7f9281f310a0a7772f97d253cb69298
eugenewisdom257-create <eugenewisdom257@gmail.com>
2026-04-23 14:06:21 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div style={{ padding: 40, fontFamily: "Arial" }}>
+    <div style={{ padding:40 }}>
       <h1>EuTech School System</h1>
       <p>The operating system for modern schools.</p>
 
-      <button style={{ padding: 10, marginTop: 20 }}>
-        Request Demo
-      </button>
+      <Link href="/login">
+        <button>Login</button>
+      </Link>
+
+      <br /><br />
+
+      <Link href="/dashboard">
+        <button>Dashboard</button>
+      </Link>
+
     </div>
   );
 }
