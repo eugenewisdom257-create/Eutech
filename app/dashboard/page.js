9d92df3048d2b11c86c97de7390bddc6538b3d0a
eugenewisdom257-create <eugenewisdom257@gmail.com>
2026-04-23 13:58:01 +0100

    Create app/dashboard/page.js

---
+++ app/dashboard/page.js
@@ -0,0 +1,13 @@
+export default function Dashboard(){
+ return (
+   <div style={{padding:40}}>
+      <h1>Admin Dashboard</h1>
+      <ul>
+        <li>Students</li>
+        <li>Fees</li>
+        <li>Documents</li>
+        <li>Messages</li>
+      </ul>
+   </div>
+ );
+}
