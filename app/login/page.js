e29542a662839c52e17ca65af4e621cf1a313775
eugenewisdom257-create <eugenewisdom257@gmail.com>
2026-04-23 21:06:59 +0100

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,39 +1,48 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "../lib/supabase";
+import { useRouter } from "next/navigation";
+
 export default function LoginPage() {
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function login() {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error.message);
+    } else {
+      router.push("/dashboard");
+    }
+  }
+
   return (
-    <div style={{
-      maxWidth: 400,
-      margin: "80px auto",
-      padding: 30,
-      border: "1px solid #ddd",
-      borderRadius: 12
-    }}>
-      <h1>EuTech Login</h1>
+    <div style={{ padding: 40 }}>
+      <h1>Login</h1>
 
       <input
-        type="email"
         placeholder="Email"
-        style={{
-          width:"100%",
-          padding:12,
-          marginTop:15
-        }}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        style={{ display:"block", margin:10, padding:10 }}
       />
 
       <input
         type="password"
         placeholder="Password"
-        style={{
-          width:"100%",
-          padding:12,
-          marginTop:15
-        }}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        style={{ display:"block", margin:10, padding:10 }}
       />
 
-      <button style={{
-        marginTop:20,
-        padding:12,
-        width:"100%"
-      }}>
+      <button onClick={login} style={{ padding:10, marginTop:10 }}>
         Login
       </button>
     </div>
