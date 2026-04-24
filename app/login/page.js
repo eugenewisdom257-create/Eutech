7786875690696ca6564995fc6a3ad0aef968b094
eugenewisdom257-create <eugenewisdom257@gmail.com>
2026-04-24 08:19:49 +0100

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,50 +1,15 @@
-"use client";
-
-import { useState } from "react";
-import { supabase } from "../lib/supabase";
-import { useRouter } from "next/navigation";
-
 export default function LoginPage() {
-  const router = useRouter();
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  async function login() {
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
-
-    if (error) {
-      alert(error.message);
-    } else {
-      router.push("/dashboard");
-    }
-  }
-
   return (
-    <div style={{ padding: 40 }}>
-      <h1>Login</h1>
+    <div style={{ padding:40 }}>
+      <h1>EuTech Login</h1>
 
-      <input
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        style={{ display:"block", margin:10, padding:10 }}
-      />
+      <input placeholder="Email" />
+      <br /><br />
 
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        style={{ display:"block", margin:10, padding:10 }}
-      />
+      <input type="password" placeholder="Password" />
+      <br /><br />
 
-      <button onClick={login} style={{ padding:10, marginTop:10 }}>
-        Login
-      </button>
+      <button>Login</button>
     </div>
   );
 }
